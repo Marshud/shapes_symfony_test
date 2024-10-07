73b7332297a3c59e91b94d5bf3f2c9e1dea5a943
marshud <ubatamye@gmail.com>
2024-10-07 13:42:36 +0300

    Serializing and fixing controller typos

--- src/Controller/GeometryController.php
+++ src/Controller/GeometryController.php
@@ -7,43 +7,55 @@ use App\Entity\Triangle;
 use App\Service\GeometryCalculator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class GeometryController extends AbstractController
 {
   private $geometryCalculator;
+  private $serializer;
 
-  public function __construct(GeometryCalculator $geometryCalculator)
+  public function __construct(GeometryCalculator $geometryCalculator, SerializerInterface $serializer)
   {
     $this->geometryCalculator = $geometryCalculator;
+    $this->serializer = $serializer;
   }
 
   public function getCircleData(float $radius): JsonResponse
   {
     $circle = new Circle($radius);
     $circle_data = $this->geometryCalculator->calculateCirlceData($circle);
-    return $this->json($circle_data);
+
+    // Serialize the data
+    $json_data =$this->serializer->serialize($circle_data, 'json');
+
+    return new JsonResponse($json_data, 200, [], true);
   }
 
   public function getTriangleData(float $a, float $b, float $c): JsonResponse
   {
     $triangle = new Triangle($a, $b, $c);
     $triangle_data = $this->geometryCalculator->calculateTriangleData($triangle);
-    return $this->json($triangle_data);
+
+    // Serialize the data
+    $json_data = $this->serializer->serialize($triangle_data, 'json');
+
+    return new JsonResponse($json_data, 200, [], true);
   }
 
-  public function sumAreas(Circle $circle, Triangle $triangle): JsonResponse
-  {
-    $sumAreas = $this->geometryCalculator->sumAreas($circle, $triangle);
-    return $this->json([
-      'sum_areas' => $sumAreas
-    ]);
-  }
+  // public function sumAreas(Circle $circle, Triangle $triangle): JsonResponse
+  // {
+  //   $sumAreas = $this->geometryCalculator->sumAreas($circle, $triangle);
+  //   return $this->json([
+  //     'sum_areas' => $sumAreas
+  //   ]);
+  // }
 
-  public function sumDiameters(Circle $circle, Triangle $triangle): JsonResponse
-  {
-    $sumDiameters = $this->geometryCalculator->sumDiameters($circle, $triangle);
-    return $this->json([
-      'sum_diameters' => $sumDiameters
-    ]);
-  }
+  // public function sumDiameters(Circle $circle, Triangle $triangle): JsonResponse
+  // {
+  //   $sumDiameters = $this->geometryCalculator->sumDiameters($circle, $triangle);
+  //   return $this->json([
+  //     'sum_diameters' => $sumDiameters
+  //   ]);
+  // }
+  
 }
