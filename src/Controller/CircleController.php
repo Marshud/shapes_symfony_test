87312bd0d54f89637900b0e9b28ab4b92b3e87b6
marshud <ubatamye@gmail.com>
2024-10-07 11:33:22 +0300

    Adding cirlce initial implemetantion

---
+++ src/Controller/CircleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class CircleController extends AbstractController
+{
+  public int $radius;
+
+  public function index(int $radius): JsonResponse
+  {
+    $this->radius = $radius;
+
+    return $this->json([
+      "type" => "cirlce",
+      "radius" => $this->radius,
+      "surface" => $this->calculateSurface(),
+      "circumference" => $this->calculateSurface()
+    ]);
+
+  }
+
+  public function calculateSurface(): int // Also the circumference
+  {
+    return 2*3.14*$this->radius;
+  }
+
+  public function CalculateDiameter(): int 
+  {
+    return 2*$this->radius;
+  }
+
+}
