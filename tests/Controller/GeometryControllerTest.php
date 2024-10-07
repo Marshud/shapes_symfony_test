60ece9a6e2ef691624a20d382d3deaecb475b606
marshud <ubatamye@gmail.com>
2024-10-07 15:04:32 +0300

    Adding unit tests

---
+++ tests/Controller/GeometryControllerTest.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GeometryControllerTest extends WebTestCase
+{
+  public function testGetCircleData()
+  {
+    // Simulate a client
+    $client = static::createClient();
+    $client->request('GET', 'http://127.0.0.1:8000/circle/2');
+
+    // Asssert a successful response
+    $this->assertResponseIsSuccessful();
+    $this->assertResponseStatusCodeSame(200);
+
+    // Verify that it's JSON
+    $this->assertResponseHeaderSame('content-type', 'application/json');
+
+    // Now we check the response data
+    $responseContent = $client->getResponse()->getContent();
+    $expectedData = '{"type":"Cirlce","radius":2.0,"surface":12.566370614359,"circumference":12.566370614359}';
+    $this->assertJsonStringEqualsJsonString($expectedData, $responseContent);
+  }
+
+  public function testTriangleData() 
+  {
+    // Simulate a client request
+    $client = static::createClient();
+    $client->request('GET', 'http://127.0.0.1:8000/triangle/3/4/5');
+
+    // Assert a successful response
+    $this->assertResponseIsSuccessful();
+    $this->assertResponseStatusCodeSame(200);
+
+    // Verify that it's JSON
+    $this->assertResponseHeaderSame('content-type', 'application/json');
+
+    // Now we check the response data
+    $responseContent = $client->getResponse()->getContent();
+    $expectedData = '{"type":"Triangle","a":3.0,"b":4.0,"b":5.0,"surface":6,"circumference":12}';
+    $this->assertJsonStringEqualsJsonString($expectedData, $responseContent);
+
+  }
+}
